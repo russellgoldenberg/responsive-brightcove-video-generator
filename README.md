afe0bde91e82b169e6922c268dd89fafd6ff8048
russell goldenberg <russellgoldenberg@gmail.com>
2014-12-19 12:23:05 -0500

    done

---
+++ README.md
@@ -0,0 +1,13 @@
+Responsive Table Generator
+==============================
+
+[Try it now](http://russellgoldenberg.github.io/responsive-table-generator)
+
+### Description
+HTML/CSS only responsive table generator for your next table.
+
+### Browser compatibility
+Probably everything.
+
+### In the wild
+[A Boston Globe example](http://www.bostonglobe.com/sports/2014/12/17/tom-brady-rob-gronkowski-top-after-fan-pro-bowl-voting/OEIeVE3h9dPbo8b6Y5izkI/story.html)
---
+++ responsive-video.js
@@ -0,0 +1,47 @@
+(function() {
+
+	function init() {
+		bindEvents();
+	}
+
+	function bindEvents() {
+		$('.generate-code').on('click', function() {
+			var id = $('.input-video-id').val().trim();
+			if(id.length > 0) {
+				generateCode(id);	
+			}
+		});
+	}
+
+	function generateCode(id) {
+		var html = '';
+
+		var css = '.outer-video-container {width: 100%;max-width: 960px;margin: 1em auto;}.inner-video-container {position: relative;height: 0;overflow: hidden;padding-bottom: 56.25%;}object.BrightcoveExperience {position: absolute;top: 0;left: 0;width: 100%;height: 100%;}';
+
+		html += '<style>' + css + '</style>';
+
+		html += '<div class="outer-video-container">';
+		html += '<div class="inner-video-container">';
+		html += '<object id="myExperience' + id + '" class="BrightcoveExperience">';
+		html += '<param name="bgcolor" value="#FFFFFF" />';
+		html += '<param name="width" value="640" />';
+		html += '<param name="height" value="480" />';
+		html += '<param name="playerID" value="876399703001" />';
+		html += '<param name="playerKey" value="AQ~~,AAAAAA6piHY~,DqRT40XOAr8ZcUAbMBHT4FlxXBkSQ2C2" />';
+		html += '<param name="isVid" value="true" />';
+		html += '<param name="isUI" value="true" />';
+		html += '<param name="dynamicStreaming" value="true" />';
+		html += '<param name="@videoPlayer" value="' + id + '" />';
+		html += '</object>';
+		html += '</div>';
+		html += '</div>';
+		html += '<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>';
+
+		$('.preview').removeClass('hide');
+		$('.preview .demo').empty().html(html);
+		$('.output-code').val(html);
+	}
+
+
+	init();
+})();
